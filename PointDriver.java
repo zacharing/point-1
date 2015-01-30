e65dd789363f615604da8622f0fd54dfa02b95c7
zacharing <zaw9998@saintjoe.edu>
2015-01-30 15:45:10 -0500

    Test new method in Point class#

--- PointDriver.java
+++ PointDriver.java
@@ -1,19 +1,33 @@
+/* Driver for Point class
+	Zachary Aring
+	30 January 2015
+*/
+
+
 public class PointDriver {
 
+	// Create three points
 	public static void main(String[] args ) {
 	Point lowerLeft = new Point();
 	Point upperRight = new Point();
 	Point middlePoint = new Point();
 
+	// Give them some values
 	lowerLeft.x = 0.0;
 	lowerLeft.y = 0.0;
 
+	
 	upperRight.x = 1280.0;
 	upperRight.y = 1024.0;
 
 	middlePoint.x = 640.0;
 	middlePoint.y = 512.0;
 
-	lowerLeft.printPoint();
+	// Print points
+
+	upperRight.printPoint();
+	// Clear po
+	upperRight.clear();	
+	upperRight.printPoint();	
 	}
 }
