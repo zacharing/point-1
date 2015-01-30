cff914e51d780dde98721f023d0425a8fbaa64dc
zacharing <zaw9998@saintjoe.edu>
2015-01-30 15:44:19 -0500

    Add clear() method to point#

--- Point.java
+++ Point.java
@@ -1,8 +1,20 @@
+/* Point class for CMP112
+	Zachary Aring
+	30 January 2015
+*/
+
 public class Point {
 	public double x,y;
 
+	// Print out the coordinates for a single point
 	public void printPoint() {
 	  System.out.println("X coordinate: " +  this.x);
 	  System.out.println("Y coordinate: " +  this.y);
+	}
+
+	// Clear x and y values (i.e reset point to 0)
+	public void clear() {
+	  x = 0.0;
+	  y = 0.0;
 	}
 }
