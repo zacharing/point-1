8f33e31d71fed9f2520a2e993c4cd099c4728ccd
zacharing <zaw9998@saintjoe.edu>
2015-02-04 15:31:01 -0500

    for my personal use.#

--- Point.java
+++ Point.java
@@ -1,6 +1,6 @@
-/* Point class for CMP112
-	Zachary Aring
-	30 January 2015
+/* Point class for CMP112 
+	Brian Capouch
+	30 January 2015 
 */
 
 public class Point {
@@ -11,10 +11,17 @@ public class Point {
 	  System.out.println("X coordinate: " +  this.x);
 	  System.out.println("Y coordinate: " +  this.y);
 	}
-
-	// Clear x and y values (i.e reset point to 0)
+	
+	// Clear x and y values (i.e. reset point to 0)
 	public void clear() {
 	  x = 0.0;
 	  y = 0.0;
+	}
+	
+	// Calculate distance between two points
+	public double distance(Point that) {
+	  double xdiff = x - that.x;
+	  double ydiff = y - that.y;
+	  return Math.sqrt(xdiff * xdiff + ydiff * ydiff);
 	}
 }
